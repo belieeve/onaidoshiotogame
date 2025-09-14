d7740708cf9f8cb4d106029e635d5a9d8531d61d
belieeve <215941085+belieeve@users.noreply.github.com>
2025-09-14 21:23:37 +0900

    chore(pages): add docs/ with built site as alternative Pages source

---
+++ docs/sw.js
@@ -0,0 +1,31 @@
+/* Basic service worker for offline cache */
+const CACHE = 'djrp-cache-v1';
+const CORE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      const resClone = res.clone();
+      caches.open(CACHE).then((c) => c.put(req, resClone));
+      return res;
+    }).catch(() => cached))
+  );
+});
